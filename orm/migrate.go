17f441d001d13517b240d16fb5c9e0fd2779bd78
magacy <chenjianlin@ydjdev.com>
2022-03-16 14:49:07 +0800

    create table not exist

--- orm/migrate.go
+++ orm/migrate.go
@@ -2,6 +2,7 @@ package orm
 
 import (
     "errors"
+    "fmt"
     "reflect"
     "strings"
 )
@@ -11,6 +12,9 @@ const uniqueKeyPrefix = "keyu"
 const keyPrefix = "key"
 const nullPrefix = "null"
 const autoIncrementPrefix = "ai"
+const createdAtColumn = "created_at"
+const updatedAtColumn = "updated_at"
+const deletedAtColumn = "deleted_at"
 
 type dBColumn struct {
     Name          string // `id`
@@ -30,16 +34,35 @@ type dBColumn struct {
 func (m *Query) Migrate() error {
     db := m.DB()
     if db == nil {
-        return errors.New("db not exist")
+        return errors.New("no db exist")
     }
 
-    if len(m.tables) == 0 || len(m.tables[0].jsonFields) == 0 {
-        return errors.New("table not exist")
+    if len(m.tables) == 0 || len(m.tables[0].jsonFields) == 0 ||
+        m.tables[0].table == nil || m.tables[0].table.TableName() == "" {
+        return errors.New("no table exist")
     }
 
-    createTableSql := ""
+    dbColums := m.getMigrateColumns(m.tables[0])
+    if len(dbColums) == 0 {
+        return errors.New("no column exist")
+    }
 
-    table := m.tables[0]
+    dbColumnStrs := m.generateColumnStrings(dbColums)
+
+    createTableSql := "create table IF NOT EXISTS  `%s` (%s)"
+
+    createTableSql = fmt.Sprintf(createTableSql, m.tables[0].table.TableName(), strings.Join(dbColumnStrs, ","))
+
+    _, err := db.Exec(createTableSql)
+    return err
+}
+
+func (m *Query) generateColumnStrings(dbColums []dBColumn) []string {
+    return nil
+}
+
+func (m *Query) getMigrateColumns(table *queryTable) []dBColumn {
+    var ret []dBColumn
     for i := 0; i < table.tableStruct.NumField(); i++ {
         varField := table.tableStruct.Field(i)
 
@@ -68,8 +91,8 @@ func (m *Query) Migrate() error {
             column.Primary = true
         }
 
-        column.Default = table.getTags(i, "default")[0]
         column.Comment = table.getTags(i, "comment")[0]
+        column.Default = table.getTags(i, "default")[0]
 
         ormTags := table.getTags(i, "orm")
         if ormTags[0] != "" {
@@ -98,19 +121,47 @@ func (m *Query) Migrate() error {
             }
         }
         if column.Type == "" {
-            switch columnKind {
-            case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-                column.Type = "bigint"
-            case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-                column.Type = "bigint unsigned"
-            case reflect.String:
-                column.Type = "varchar(255)"
-            default:
-                column.Type = "varchar(255)"
+            if column.Name == createdAtColumn {
+                column.Type = "timestamp"
+                if column.Default == "" {
+                    column.Default = "CURRENT_TIMESTAMP"
+                }
+            } else if column.Name == updatedAtColumn {
+                column.Type = "timestamp"
+                if column.Default == "" {
+                    column.Default = "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+                }
+            } else if column.Name == deletedAtColumn {
+                column.Type = "timestamp"
+                column.Null = true
+                column.Default = "Null"
+            } else {
+                switch columnKind {
+                case reflect.Bool, reflect.Int8:
+                    column.Type = "tinyint"
+                case reflect.Int16:
+                    column.Type = "smallinit"
+                case reflect.Int, reflect.Int32:
+                    column.Type = "int"
+                case reflect.Int64:
+                    column.Type = "bigint"
+                case reflect.Uint8:
+                    column.Type = "tinyint unsigned"
+                case reflect.Uint16:
+                    column.Type = "smallinit unsigned"
+                case reflect.Uint, reflect.Uint32:
+                    column.Type = "int unsigned"
+                case reflect.Uint64:
+                    column.Type = "bigint unsigned"
+                case reflect.String:
+                    column.Type = "varchar(255)"
+                default:
+                    column.Type = "varchar(255)"
+                }
             }
         }
+        ret = append(ret, column)
     }
 
-    _, err := db.Exec(createTableSql)
-    return err
+    return ret
 }
