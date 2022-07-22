9fdc6d645e62d85f7f57bc5799356cda405d5b01
folospace <folospace@gmail.com>
2022-07-22 16:22:50 +0800

    test

--- orm_select_test.go
+++ orm_select_test.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"database/sql"
-	"github.com/folospace/go-mysql-orm/orm"
-	"testing"
+    "database/sql"
+    "github.com/folospace/go-mysql-orm/orm"
+    _ "github.com/go-sql-driver/mysql"
+    "testing"
 )
 
 var tdb, _ = sql.Open("mysql", "rfamro@tcp(mysql-rfam-public.ebi.ac.uk:4497)/Rfam?parseTime=true&charset=utf8mb4&loc=Asia%2FShanghai")
 
 func TestSelect(t *testing.T) {
-	t.Run("query_raw", func(t *testing.T) {
-		data, query := orm.NewQueryRaw(tdb, "family").Limit(5).GetRows()
-		t.Log(data)
-		t.Log(query.Sql())
-		t.Log(query.Error())
-	})
+    t.Run("query_raw", func(t *testing.T) {
+        data, query := orm.NewQueryRaw(tdb, "family").Limit(5).GetRows()
+        t.Log(data)
+        t.Log(query.Sql())
+        t.Log(query.Error())
+    })
 }
