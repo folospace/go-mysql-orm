527ab01db16443b0486544d9561bd404195e5f71
folospace <folospace@gmail.com>
2023-02-23 10:23:11 +0800

    add example

--- example.go
+++ example.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
-	"fmt"
-	"github.com/folospace/go-mysql-orm/orm"
-	_ "github.com/go-sql-driver/mysql"
+    "database/sql"
+    "errors"
+    "fmt"
+    "github.com/folospace/go-mysql-orm/orm"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 //connect mysql db
@@ -15,130 +15,122 @@ var db, _ = sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/mydb?parseTime=
 var UserTable = orm.NewQuery(User{}, db)
 
 type User struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+    Id   int    `json:"id"`
+    Name string `json:"name"`
 }
 
 func (User) TableName() string {
-	return "user"
+    return "user"
 }
 func (User) DatabaseName() string {
-	return "mydb"
+    return "mydb"
 }
 
 //user table model
 var OrderTable = orm.NewQuery(Order{}, db)
 
 type Order struct {
-	Id          int `json:"id"`
-	UserId      int `json:"user_id"`
-	OrderAmount int `json:"order_amount"`
+    Id          int `json:"id"`
+    UserId      int `json:"user_id"`
+    OrderAmount int `json:"order_amount"`
 }
 
 func (Order) TableName() string {
-	return "order"
+    return "order"
 }
 func (Order) DatabaseName() string {
-	return "mydb"
+    return "mydb"
 }
 
 func main() {
-	{
-		//create db table from go struct
-		_, _ = orm.CreateTableFromStruct(UserTable)
-		//create go struct from db table
-		_ = orm.CreateStructFromTable(UserTable)
-	}
+    {
+        //create db table from go struct
+        _, _ = orm.CreateTableFromStruct(UserTable)
+        //create go struct from db table
+        _ = orm.CreateStructFromTable(UserTable)
+    }
 
-	//query select
-	{
-		//get first user (name='join') as struct
-		user, query := UserTable.Where(&UserTable.T.Name, "john").Get()
-		fmt.Println(user, query.Sql(), query.Error())
+    //query select
+    {
+        //get first user (name='join') as struct
+        user, query := UserTable.Where(&UserTable.T.Name, "john").Get()
+        fmt.Println(user, query.Sql(), query.Error())
 
-		//get user where primary id = 1
-		user, query = UserTable.Get(1)
-		fmt.Println(user, query.Sql(), query.Error())
+        //get users by primary ids
+        users, query := UserTable.Gets(1, 2, 3)
+        fmt.Println(users, query.Sql(), query.Error())
 
-		//get users as struct slice
-		users, query := UserTable.Limit(5).Gets()
-		fmt.Println(users, query.Sql(), query.Error())
+        //equals get users id in (1,2,3)
+        users, query = UserTable.WherePrimary(1, 2, 3).Gets()
+        fmt.Println(users, query.Sql(), query.Error())
 
-		//get users by primary ids
-		users, query = UserTable.Gets(1, 2, 3)
-		fmt.Println(users, query.Sql(), query.Error())
+        //get user rows as []map[string]interface
+        rows, query := UserTable.Limit(5).GetRows()
+        fmt.Println(rows, query.Sql(), query.Error())
 
-		//get user first row as map[string]interface
-		row, query := UserTable.GetRow()
-		fmt.Println(row, query.Sql(), query.Error())
+        //get users count(*)
+        count, query := UserTable.GetCount()
+        fmt.Println(count, query.Sql(), query.Error())
 
-		//get user rows as []map[string]interface
-		rows, query := UserTable.Limit(5).GetRows()
-		fmt.Println(rows, query.Sql(), query.Error())
+        //get user names map key by id
+        var userNameKeyById map[int]string
+        UserTable.Select(&UserTable.T.Id, &UserTable.T.Name).GetTo(&userNameKeyById)
 
-		//get users count(*)
-		count, query := UserTable.GetCount()
-		fmt.Println(count, query.Sql(), query.Error())
+        //get users map key by name
+        var usersMapkeyByName map[string][]User
+        UserTable.Select(&UserTable.T.Name, UserTable.AllCols()).GetTo(&usersMapkeyByName)
 
-		//get users map key by id
-		var usersKeyById map[int]User
-		UserTable.GetTo(&usersKeyById)
+        //select rank by column
+        OrderTable.Where(&OrderTable.T.UserId, 1).
+            Select(OrderTable.AllCols()).
+            SelectRank(&OrderTable.T.OrderAmount, "order_amount_rank").GetRows()
+    }
 
-		//get user names map key by id
-		var userNameKeyById map[int]string
-		UserTable.Select(&UserTable.T.Id, &UserTable.T.Name).GetTo(&userNameKeyById)
+    //query update and delete and insert
+    {
+        //update user set name="hello" where id=1
+        UserTable.WherePrimary(1).Update(&UserTable.T.Name, "hello")
 
-		//get users map key by name
-		var usersMapkeyByName map[string][]User
-		UserTable.Select(&UserTable.T.Name, UserTable.AllCols()).GetTo(&usersMapkeyByName)
+        //query delete
+        UserTable.Delete(1, 2, 3)
 
-		//select orders (where user_id=1) with rank by order_amount
-		OrderTable.Where(&OrderTable.T.UserId, 1).
-			Select(OrderTable.AllCols()).
-			SelectRank(&OrderTable.T.OrderAmount, "order_amount_rank").GetRows()
-	}
+        //query insert
+        _ = UserTable.Insert(User{Name: "han"}).LastInsertId //insert one row and get id
 
-	//query update and delete and insert
-	{
-		//update user set name="hello" where id=1
-		UserTable.Where(&UserTable.T.Id, 1).Update(&UserTable.T.Name, "hello")
+        //insert batch on duplicate key update name=values(name)
+        _ = UserTable.InsertsIgnore([]User{{Id: 1, Name: "han"}, {Id: 2, Name: "jen"}},
+            []orm.UpdateColumn{{Column: &UserTable.T.Name, Val: &UserTable.T.Name}})
+    }
 
-		//query delete
-		UserTable.Delete(1, 2, 3)
+    //query join
+    {
+        UserTable.Join(OrderTable.T, func(query orm.Query[User]) orm.Query[User] {
+            return query.Where(&UserTable.T.Id, &OrderTable.T.UserId)
+        }).Where(&OrderTable.T.OrderAmount, 100).
+            Select(UserTable.AllCols()).Gets()
+    }
+    {
+        //transaction
+        _ = UserTable.Transaction(func(tx *sql.Tx) error {
+            newId := UserTable.UseTx(tx).Insert(User{Name: "john"}).LastInsertId //insert
+            fmt.Println(newId)
+            return errors.New("I want rollback") //rollback
+        })
+    }
 
-		//query insert
-		_ = UserTable.Insert(User{Name: "han"}).LastInsertId //insert one row and get id
-	}
+    {
+        //subquery
+        subquery := UserTable.Where(&UserTable.T.Id, 1).SubQuery()
 
-	//query join
-	{
-		UserTable.Join(OrderTable.T, func(query orm.Query[User]) orm.Query[User] {
-			return query.Where(&UserTable.T.Id, &OrderTable.T.UserId)
-		}).Where(&OrderTable.T.OrderAmount, 100).
-			Select(UserTable.AllCols()).Gets()
-	}
-	{
-		//transaction
-		_ = UserTable.Transaction(func(tx *sql.Tx) error {
-			newId := UserTable.UseTx(tx).Insert(User{Name: "john"}).LastInsertId //insert
-			fmt.Println(newId)
-			return errors.New("I want rollback") //rollback
-		})
-	}
+        //where in suquery
+        UserTable.Where(&UserTable.T.Id, orm.WhereIn, subquery).Gets()
 
-	{
-		//subquery
-		subquery := UserTable.Where(&UserTable.T.Id, 1).SubQuery()
+        //insert subquery
+        UserTable.InsertSubquery(subquery, nil)
 
-		//where in suquery
-		UserTable.Where(&UserTable.T.Id, orm.WhereIn, subquery).Gets()
-
-		//insert subquery
-		UserTable.InsertSubquery(subquery, nil)
-
-		//join subquery
-		UserTable.Join(subquery, func(query orm.Query[User]) orm.Query[User] {
-			return query.Where(&UserTable.T.Id, orm.Raw("sub.id"))
-		}).Gets()
-	}
+        //join subquery
+        UserTable.Join(subquery, func(query orm.Query[User]) orm.Query[User] {
+            return query.Where(&UserTable.T.Id, orm.Raw("sub.id"))
+        }).Gets()
+    }
 }
--- orm/query_insert.go
+++ orm/query_insert.go
@@ -1,193 +1,193 @@
 package orm
 
 import (
-	"database/sql"
-	"errors"
-	"reflect"
-	"strings"
+    "database/sql"
+    "errors"
+    "reflect"
+    "strings"
 )
 
 //tableFieldPtrs: allow insert table columns
 func (m Query[T]) Insert(data T, tableFieldPtrs ...interface{}) QueryResult {
-	return m.insert(false, []T{data}, tableFieldPtrs, nil)
+    return m.insert(false, []T{data}, tableFieldPtrs, nil)
 }
 
 //tableFieldPtrs: allow insert table columns
 func (m Query[T]) Inserts(data []T, tableFieldPtrs ...interface{}) QueryResult {
-	return m.insert(false, data, tableFieldPtrs, nil)
+    return m.insert(false, data, tableFieldPtrs, nil)
 }
 
 //insert ignore ... // on duplicate key update ...
-func (m Query[T]) InsertsIgnore(data []T, tableFieldPtrs []interface{}, updates ...UpdateColumn) QueryResult {
-	return m.insert(true, data, tableFieldPtrs, updates)
+func (m Query[T]) InsertsIgnore(data []T, updates []UpdateColumn, tableFieldPtrs ...interface{}) QueryResult {
+    return m.insert(true, data, tableFieldPtrs, updates)
 }
 
 func (m Query[T]) InsertSubquery(data SubQuery, tableFieldPtrs []interface{}, updates ...UpdateColumn) QueryResult {
-	return m.insert(true, data, tableFieldPtrs, updates)
+    return m.insert(true, data, tableFieldPtrs, updates)
 }
 
 func (m Query[T]) gennerateInsertSql(InsertColumns []string, rowCount int) string {
-	columnRawStr := ""
-	valRawStr := ""
+    columnRawStr := ""
+    valRawStr := ""
 
-	if len(InsertColumns) > 0 {
-		for _, v := range InsertColumns {
-			columnRawStr += "`" + v + "`,"
-			valRawStr += "?,"
-		}
-		columnRawStr = "(" + strings.TrimRight(columnRawStr, ",") + ")"
-		valRawStr = "(" + strings.TrimRight(valRawStr, ",") + ")"
-	}
+    if len(InsertColumns) > 0 {
+        for _, v := range InsertColumns {
+            columnRawStr += "`" + v + "`,"
+            valRawStr += "?,"
+        }
+        columnRawStr = "(" + strings.TrimRight(columnRawStr, ",") + ")"
+        valRawStr = "(" + strings.TrimRight(valRawStr, ",") + ")"
+    }
 
-	if rowCount > 0 {
-		rows := make([]string, rowCount)
-		for k := range rows {
-			rows[k] = valRawStr
-		}
-		return columnRawStr + " values " + strings.Join(rows, ",")
-	} else {
-		return columnRawStr
-	}
+    if rowCount > 0 {
+        rows := make([]string, rowCount)
+        for k := range rows {
+            rows[k] = valRawStr
+        }
+        return columnRawStr + " values " + strings.Join(rows, ",")
+    } else {
+        return columnRawStr
+    }
 }
 
 func (m Query[T]) getInsertBindings(val reflect.Value, isSlice bool, validFieldIndex map[int]struct{}, defaults map[int]interface{}) []interface{} {
-	var bindings []interface{}
-	if isSlice == false {
-		for i := 0; i < val.NumField(); i++ {
-			if _, ok := validFieldIndex[i]; ok {
-				if defaults[i] != nil && val.Field(i).IsZero() {
-					bindings = append(bindings, defaults[i])
-				} else {
-					bindings = append(bindings, val.Field(i).Interface())
-				}
-			}
-		}
-	} else {
-		for i := 0; i < val.Len(); i++ {
-			for k := 0; k < val.Index(i).NumField(); k++ {
-				if _, ok := validFieldIndex[k]; ok {
-					if defaults[k] != nil && val.Index(i).Field(k).IsZero() {
-						bindings = append(bindings, defaults[k])
-					} else {
-						bindings = append(bindings, val.Index(i).Field(k).Interface())
-					}
-				}
-			}
-		}
-	}
-	return bindings
+    var bindings []interface{}
+    if isSlice == false {
+        for i := 0; i < val.NumField(); i++ {
+            if _, ok := validFieldIndex[i]; ok {
+                if defaults[i] != nil && val.Field(i).IsZero() {
+                    bindings = append(bindings, defaults[i])
+                } else {
+                    bindings = append(bindings, val.Field(i).Interface())
+                }
+            }
+        }
+    } else {
+        for i := 0; i < val.Len(); i++ {
+            for k := 0; k < val.Index(i).NumField(); k++ {
+                if _, ok := validFieldIndex[k]; ok {
+                    if defaults[k] != nil && val.Index(i).Field(k).IsZero() {
+                        bindings = append(bindings, defaults[k])
+                    } else {
+                        bindings = append(bindings, val.Index(i).Field(k).Interface())
+                    }
+                }
+            }
+        }
+    }
+    return bindings
 }
 
 func (m Query[T]) insert(ignore bool, data interface{}, tableFieldPtrs []interface{}, updates []UpdateColumn) QueryResult {
-	val := reflect.ValueOf(data)
-	var err error
-	isSlice := false
-	isSubQuery := false
-	var subq SubQuery
-	rowCount := 1
-	var structFields []string
-	var structDefaults map[int]interface{}
-	if val.Kind() == reflect.Slice {
-		rowCount = val.Len()
-		if rowCount == 0 {
-			m.setErr(errors.New("slice is empty"))
-			return m.result
-		}
-		if val.Index(0).Kind() != reflect.Struct {
-			m.setErr(errors.New("slice elem must be struct"))
-		} else {
-			isSlice = true
-			structFields, err = getStructFieldNameSlice(val.Index(0).Interface())
-			m.setErr(err)
-			structDefaults, err = getStructFieldWithDefaultTime(val.Index(0).Interface())
-			m.setErr(err)
-		}
-	} else if val.Kind() == reflect.Struct {
-		sub, ok := data.(SubQuery)
-		if ok == false {
-			structFields, err = getStructFieldNameSlice(data)
-			m.setErr(err)
-			structDefaults, err = getStructFieldWithDefaultTime(val.Index(0).Interface())
-			m.setErr(err)
-		} else {
-			isSubQuery = true
-			subq = sub
-		}
-	} else {
-		m.setErr(errors.New("data must be struct or slice of struct"))
-	}
+    val := reflect.ValueOf(data)
+    var err error
+    isSlice := false
+    isSubQuery := false
+    var subq SubQuery
+    rowCount := 1
+    var structFields []string
+    var structDefaults map[int]interface{}
+    if val.Kind() == reflect.Slice {
+        rowCount = val.Len()
+        if rowCount == 0 {
+            m.setErr(errors.New("slice is empty"))
+            return m.result
+        }
+        if val.Index(0).Kind() != reflect.Struct {
+            m.setErr(errors.New("slice elem must be struct"))
+        } else {
+            isSlice = true
+            structFields, err = getStructFieldNameSlice(val.Index(0).Interface())
+            m.setErr(err)
+            structDefaults, err = getStructFieldWithDefaultTime(val.Index(0).Interface())
+            m.setErr(err)
+        }
+    } else if val.Kind() == reflect.Struct {
+        sub, ok := data.(SubQuery)
+        if ok == false {
+            structFields, err = getStructFieldNameSlice(data)
+            m.setErr(err)
+            structDefaults, err = getStructFieldWithDefaultTime(val.Index(0).Interface())
+            m.setErr(err)
+        } else {
+            isSubQuery = true
+            subq = sub
+        }
+    } else {
+        m.setErr(errors.New("data must be struct or slice of struct"))
+    }
 
-	if m.result.Err != nil {
-		return m.result
-	}
+    if m.result.Err != nil {
+        return m.result
+    }
 
-	var validFieldNameMap = make(map[string]struct{})
-	var validFieldNames = make([]string, 0)
-	var validFieldIndex = make(map[int]struct{})
-	var InsertColumns []string //actually insert columns
-	var allowFields = make(map[interface{}]struct{})
+    var validFieldNameMap = make(map[string]struct{})
+    var validFieldNames = make([]string, 0)
+    var validFieldIndex = make(map[int]struct{})
+    var InsertColumns []string //actually insert columns
+    var allowFields = make(map[interface{}]struct{})
 
-	for _, v := range tableFieldPtrs {
-		allowFields[v] = struct{}{}
-	}
-	for k, v := range m.tables[0].ormFields {
-		_, ok := allowFields[k]
-		if ok || (len(allowFields) == 0 && isSubQuery == false) {
-			validFieldNameMap[v] = struct{}{}
-			validFieldNames = append(validFieldNames, v)
-		}
-	}
+    for _, v := range tableFieldPtrs {
+        allowFields[v] = struct{}{}
+    }
+    for k, v := range m.tables[0].ormFields {
+        _, ok := allowFields[k]
+        if ok || (len(allowFields) == 0 && isSubQuery == false) {
+            validFieldNameMap[v] = struct{}{}
+            validFieldNames = append(validFieldNames, v)
+        }
+    }
 
-	var insertSql, updateStr string
-	var bindings []interface{}
-	if isSubQuery {
-		insertSql = m.gennerateInsertSql(validFieldNames, 0)
-		if insertSql != "" {
-			insertSql += " "
-		}
-		insertSql += subq.raw
-		bindings = subq.bindings
-	} else {
-		for k, v := range structFields {
-			if _, ok := validFieldNameMap[v]; ok {
-				validFieldIndex[k] = struct{}{}
-				InsertColumns = append(InsertColumns, v)
-			}
-		}
-		insertSql = m.gennerateInsertSql(InsertColumns, rowCount)
-		bindings = m.getInsertBindings(val, isSlice, validFieldIndex, structDefaults)
-	}
+    var insertSql, updateStr string
+    var bindings []interface{}
+    if isSubQuery {
+        insertSql = m.gennerateInsertSql(validFieldNames, 0)
+        if insertSql != "" {
+            insertSql += " "
+        }
+        insertSql += subq.raw
+        bindings = subq.bindings
+    } else {
+        for k, v := range structFields {
+            if _, ok := validFieldNameMap[v]; ok {
+                validFieldIndex[k] = struct{}{}
+                InsertColumns = append(InsertColumns, v)
+            }
+        }
+        insertSql = m.gennerateInsertSql(InsertColumns, rowCount)
+        bindings = m.getInsertBindings(val, isSlice, validFieldIndex, structDefaults)
+    }
 
-	updateStr = m.generateUpdateStr(updates, &bindings)
+    updateStr = m.generateUpdateStr(updates, &bindings)
 
-	rawSql := "insert"
-	if ignore {
-		rawSql += " ignore"
-	}
+    rawSql := "insert"
+    if ignore {
+        rawSql += " ignore"
+    }
 
-	rawSql += " into " + m.tables[0].getTableName() + " " + insertSql
+    rawSql += " into " + m.tables[0].getTableName() + " " + insertSql
 
-	if updateStr != "" {
-		rawSql += " on duplicate key update " + updateStr
-	}
+    if updateStr != "" {
+        rawSql += " on duplicate key update " + updateStr
+    }
 
-	rawSql += ";"
-	m.result.PrepareSql = rawSql
-	m.result.Bindings = bindings
+    rawSql += ";"
+    m.result.PrepareSql = rawSql
+    m.result.Bindings = bindings
 
-	var insertRes sql.Result
-	if m.dbTx() != nil {
-		insertRes, err = m.dbTx().Exec(rawSql, bindings...)
-	} else {
-		insertRes, err = m.DB().Exec(rawSql, bindings...)
-	}
+    var insertRes sql.Result
+    if m.dbTx() != nil {
+        insertRes, err = m.dbTx().Exec(rawSql, bindings...)
+    } else {
+        insertRes, err = m.DB().Exec(rawSql, bindings...)
+    }
 
-	m.setErr(err)
-	if insertRes != nil {
-		m.result.LastInsertId, err = insertRes.LastInsertId()
-		m.setErr(err)
-		m.result.RowsAffected, err = insertRes.RowsAffected()
-		m.setErr(err)
-	}
-	return m.result
+    m.setErr(err)
+    if insertRes != nil {
+        m.result.LastInsertId, err = insertRes.LastInsertId()
+        m.setErr(err)
+        m.result.RowsAffected, err = insertRes.RowsAffected()
+        m.setErr(err)
+    }
+    return m.result
 }
